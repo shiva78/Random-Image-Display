677c1fcfaf7ff9cb1637e3c410bc7f18c6acfafe
Shiva.Magar <t.shiva88@gmail.com>
2019-02-01 12:48:25 +1000

    Fully functional application

---
+++ RandomImage/Controllers/DefaultController.cs
@@ -0,0 +1,72 @@
+﻿using RandomImage.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace RandomImage.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Images = getImages();
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(string answer, Preference model)
+        {
+            User_PreferencesEntities db = new User_PreferencesEntities();
+            string username = model.UserName;
+            if (answer == "Display History")
+            {
+                var historyData = db.Preferences.Where(x => x.UserName.Contains(username)).ToList();
+                if (historyData.Count > 0)
+                {
+                    ViewBag.HistoryData = historyData;
+                }
+                else
+                {
+                    ViewBag.NoHistory = "Sorry you don't have any history to display.";
+                }
+            }
+            else
+            {
+                string image = model.Image;
+                db.Preferences.Add(new Preference
+                {
+                    Id = model.Id,
+                    UserName = model.UserName,
+                    User_Preferences = answer,
+                    Image = model.Image
+
+                });
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    e.ToString();
+                }
+            }
+            ViewBag.Images = getImages();
+            return View();
+        }
+
+        private dynamic getImages()
+        {
+            string path = System.AppDomain.CurrentDomain.BaseDirectory + "Images";
+            var filenames = Directory
+                .EnumerateFiles(path, "*", SearchOption.AllDirectories)
+                .Select(Path.GetFileName).ToList();
+            return filenames; 
+        }
+    }
+}
---
+++ RandomImage/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RandomImage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RandomImage")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a42387f-3140-41b4-9943-e54b82aeef13")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
